0cfe4d3736c9189730e517ef3359e56f855f32b7
Xu Tian <xu_tian0907@utexas.edu>
2020-04-22 01:10:55 -0500

    Assignment 18

--- assignment18.py
+++ assignment18.py
@@ -19,6 +19,33 @@ class EpetraParallelToughness(StressStrainConverter):
         else:
             self.true_stress = np.array([], dtype=np.double)
             self.true_strain = np.array([], dtype=np.double)
+                  
+        
+        unbalanced_map = Epetra.Map(-1, self.true_stress.shape[0], 0, self.comm)
+        
+        unbalanced_stress = Epetra.Vector(Epetra.Copy, unbalanced_map, self.true_stress)
+        unbalanced_strain = Epetra.Vector(Epetra.Copy, unbalanced_map, self.true_strain)
+        
+        balanced_map = self.create_balanced_map(unbalanced_map)
+        self.true_stress = Epetra.Vector(balanced_map)
+        self.true_strain = Epetra.Vector(balanced_map)
+        
+        importer = Epetra.Import(balanced_map, unbalanced_map)
+        
+        self.true_stress.Import(unbalanced_stress, importer, Epetra.Insert)
+        self.true_strain.Import(unbalanced_strain, importer, Epetra.Insert)
+        
+    def create_balanced_map(self, unbalanced_map):
+        
+        temp_map = Epetra.Map(unbalanced_map.NumGlobalElements(), 0, self.comm)
+        
+        my_global_element_list = temp_map.MyGlobalElements()
+        
+        if self.rank < (self.size - 1):
+            my_global_element_list = np.append(my_global_element_list, [temp_map.MaxMyGID() + 1])
+            
+        return Epetra.Map(-1, list(my_global_element_list), 0, self.comm)
+
 
         
     def compute_toughness(self):
